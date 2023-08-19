37ac1ca36f3ffac24ec5fb81ebc9994c85a13bae
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-08-19 13:58:56 +0630

    container to max-w-screen-xl

--- src/App.jsx
+++ src/App.jsx
@@ -24,11 +24,11 @@ const App = () => {
         <main className='relative w-full'>
             <div className='pointer-events-none fixed inset-0 z-30' style={{background:`radial-gradient(600px at ${mousePos.x}px ${mousePos.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`}}></div>
             <div className='w-full min-h-screen' >
-                <div className="container mx-auto grid grid-cols-1 lg:grid-cols-2">
-                    <div className='h-auto lg:h-screen lg:sticky top-0 flex justify-center items-center px-0 lg:px-5 lg:px-14'>
+                <div className="max-w-screen-xl mx-auto grid grid-cols-1 lg:grid-cols-2">
+                    <div className='h-auto lg:h-screen lg:sticky top-0 flex justify-center items-center px-5 lg:px-14'>
                         <Home/>
                     </div>
-                    <div className='min-h-screen'>
+                    <div className='min-h-screen px-5 lg:px-16'>
                         <About/>
                         <Experience/>
                         <Project/>
