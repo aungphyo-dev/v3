408fe4f54a6ecf95b5fa578ede8cc2ab208fa43a
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-08-21 19:26:23 +0630

    error route define

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,25 @@ import App from './App.jsx'
 import './index.css'
 import {Provider} from "react-redux";
 import {store} from "./service/store.js";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Dashboard from "./layouts/Dashboard/Dashboard.jsx";
+import DProjects from "./layouts/Dashboard/DProjects.jsx";
+import DProjectCreate from "./layouts/Dashboard/DProjectCreate.jsx";
+import Error from "./layouts/Error/Error.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<App/>}/>
+                <Route path='/dashboard' element={<Dashboard/>}>
+                    <Route path='project/all' element={<DProjects/>}/>
+                    <Route path='project/create' element={<DProjectCreate/>}/>
+                </Route>
+                <Route path='*' element={<Error/>}/>
+            </Routes>
+        </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
